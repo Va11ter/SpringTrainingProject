4d9a1cf91dc8a575c4cc7c366a0a7c750348f5d7
ValeryP <>
2020-06-21 23:08:42 +0300

    Add support of WishList to Person

--- src/main/java/com/niit/BookStore/entiny/Person.java
+++ src/main/java/com/niit/BookStore/entiny/Person.java
@@ -1,6 +1,7 @@
 package com.niit.BookStore.entiny;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "person")
@@ -11,6 +12,12 @@ public class Person extends EntityBase {
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.PERSIST)
     @JoinColumn(name="address_id", referencedColumnName = "id")
     private Address address;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "wish_list",
+    joinColumns = @JoinColumn(name="person_id"),
+    inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private Set<Item> wishListItems;
 
     @Override
     public String toString() {
@@ -40,5 +47,21 @@ public class Person extends EntityBase {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Set<Item> getWishListItems() {
+        return wishListItems;
+    }
+
+    public void setWishListItems(Set<Item> wishListItems) {
+        this.wishListItems = wishListItems;
     }
 }

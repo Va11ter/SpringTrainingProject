69bc541dbe30f04357c9e5dccd52dd547fe14f82
ValeryP <>
2020-06-21 23:08:59 +0300

    Fix code Stile in PersonRepository

--- src/main/java/com/niit/BookStore/repository/PersonRepository.java
+++ src/main/java/com/niit/BookStore/repository/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.niit.BookStore.repository;
 
-
 import com.niit.BookStore.entiny.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }

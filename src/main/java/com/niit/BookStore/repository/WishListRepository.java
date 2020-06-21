7b8612c0dcee6e975f4c5dc88b9cddfed79fcb3d
ValeryP <>
2020-06-21 23:01:48 +0300

    Get rid of Wish List

--- src/main/java/com/niit/BookStore/repository/WishListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.niit.BookStore.repository;
-
-import com.niit.BookStore.entiny.WishList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WishListRepository extends JpaRepository<WishList, Long> {
-}

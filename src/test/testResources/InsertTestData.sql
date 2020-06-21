67c7ce7fb7bc7c8fb0bb80e4a06119ee046ec2c8
ValeryP <>
2020-06-21 23:16:52 +0300

    Add MySql statements for create Scheme and insert test data

---
+++ src/test/testResources/InsertTestData.sql
@@ -0,0 +1,61 @@
+USE book_store;
+
+INSERT INTO address (country, city, street, building, apartment, info, postal_code)
+VALUES ('Russia', 'N.Novgorod', 'Udmurdskaya', '1', '1', 'Please call before came', '603000'),
+       ('Russia', 'N.Novgorod', 'Rozdestvenskaya', '2', '1', 'Please bring Items only after 12:00 am', '603000'),
+       ('USA', 'Houston', 'Main', '5', '2', 'We have a problem', '77018');
+
+INSERT into person (first_name, last_name, email)
+VALUES
+       ('first_name0', 'last_name0', 'last_name0@mail.com'),
+       ('first_name1', 'last_name1', 'last_name1@mail.com'),
+       ('first_name2', 'last_name2', 'last_name2@mail.com'),
+       ('first_name3', 'last_name3', 'last_name3@mail.com');
+
+
+INSERT INTO item (name, price, description, count)
+VALUES ('NewItem', 100.00, 'Some very important description', 2),
+       ('NewItem2', 100.00, 'Some very important description2', 5),
+       ('The best NewItem2', 100.00, 'Some very important description5', 10),
+       ('Telephone S1', 100.00, 'Some very important description5', 10),
+       ('Telephone S2', 200.00, 'Some very important description5', 9),
+       ('Telephone S3', 300.00, 'Some very important description5', 8),
+       ('Telephone S4', 400.00, 'Some very important description5', 7);
+
+INSERT INTO `order` (person_id, address_id, state, total, placed_on)
+VALUES (1, 1, 'PLACED', 500, UTC_TIMESTAMP()),
+       (1, 2, 'COMPLETED', 0,  UTC_TIMESTAMP()),
+       (3, 3, 'CANCELED', 0,  UTC_TIMESTAMP());
+
+INSERT INTO order_item (order_id, item_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (3, 2),
+       (2,1);
+
+INSERT INTO cart (person_id)
+VALUES (1),(2),(3),(4);
+
+INSERT INTO cart_item (cart_id, item_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (3, 2),
+       (2, 1);
+
+INSERT INTO promo (code, start_date, end_date, discount)
+VALUES ('MayDay', DATE_SUB(NOW(), INTERVAL 1 MONTH ), DATE_ADD(NOW(), INTERVAL 1 MONTH ), 2),
+       ('MegaSail',DATE_SUB(NOW(), INTERVAL 1 MONTH ), DATE_ADD(NOW(), INTERVAL 1 MONTH ), 5),
+       ('t_oidar', DATE_SUB(NOW(), INTERVAL 1 MONTH ), DATE_ADD(NOW(), INTERVAL 1 MONTH ), 10);
+
+INSERT INTO wish_list (person_id, item_id)
+VALUES (2, 4),
+       (1, 2),
+       (1, 3);
+
+select * from promo;
+
+
+
+
---
+++ src/test/testResources/PrepareDB.sql
@@ -0,0 +1,131 @@
+/*Tested on Mysql 8.0*/
+
+CREATE DATABASE IF NOT EXISTS book_store;
+USE book_store;
+
+CREATE TABLE IF NOT EXISTS address(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    postal_code VARCHAR(10),
+    country VARCHAR(60),
+    city VARCHAR(85),
+    street VARCHAR(100),
+    building VARCHAR(25),
+    apartment VARCHAR(25),
+    info varchar(256)
+);
+
+CREATE TABLE IF NOT EXISTS person(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    address_id INT,
+
+    CONSTRAINT uniq_email UNIQUE (email),
+
+    CONSTRAINT
+        FOREIGN KEY (address_id)
+            REFERENCES address (id)
+            ON DELETE SET NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS item(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    price DECIMAL,
+    description TEXT,
+    count INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `order`(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    person_id INT NOT NULL,
+    address_id INT,
+    state VARCHAR(10),
+    total DECIMAL,
+    placed_on timestamp,
+
+    INDEX (person_id),
+    FOREIGN KEY (person_id)
+        REFERENCES person(id),
+
+    FOREIGN KEY (address_id)
+        REFERENCES address(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_item(
+    no INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
+    item_price DECIMAL,
+    item_count INT default 1,
+
+    INDEX (order_id, item_id),
+    INDEX (order_id),
+
+    FOREIGN KEY (order_id)
+        REFERENCES `order` (id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+
+    FOREIGN KEY (item_id)
+        REFERENCES item(id)
+);
+
+CREATE TABLE IF NOT EXISTS promo(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(25),
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    active boolean default true,
+    discount SMALLINT,
+    description TEXT,
+
+    CONSTRAINT unique_code UNIQUE (code)
+);
+
+CREATE TABLE IF NOT EXISTS wish_list(
+    person_id INT,
+    item_id INT,
+
+    primary key (person_id, item_id),
+    FOREIGN KEY (person_id)
+        REFERENCES person(id),
+    FOREIGN KEY (item_id)
+        REFERENCES item(id)
+);
+
+CREATE TABLE IF NOT EXISTS cart(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    person_id INT,
+    promo_id INT,
+
+    FOREIGN KEY (person_id) REFERENCES
+        person(id),
+
+    FOREIGN KEY (promo_id) REFERENCES
+        promo(id),
+
+    CONSTRAINT unique_person_id UNIQUE (person_id)
+);
+
+CREATE TABLE IF NOT EXISTS cart_item (
+    no INT NOT NULL AUTO_INCREMENT,
+    cart_id INT NOT NULL,
+    item_id INT NOT NULL,
+    count INT default 1,
+
+    PRIMARY KEY (`no`),
+    INDEX (cart_id, item_id),
+    INDEX (item_id),
+
+    FOREIGN KEY (cart_id)
+        REFERENCES cart (id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+
+    FOREIGN KEY (item_id)
+        REFERENCES item(id)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+

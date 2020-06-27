e9ddf2d14935d466a956b04f626572e78070f7d0
ValeryP <>
2020-06-27 14:12:59 +0300

    Specify name for columns in Person Entity

--- src/main/java/com/niit/BookStore/entiny/Person.java
+++ src/main/java/com/niit/BookStore/entiny/Person.java
@@ -13,7 +13,9 @@ import java.util.Set;
 @AllArgsConstructor
 @ToString(exclude = {"address"})
 public class Person extends EntityBase {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.PERSIST)

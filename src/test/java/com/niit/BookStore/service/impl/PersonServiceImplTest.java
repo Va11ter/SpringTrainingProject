7b690dc99244a32f4ef8ee78f218fc8cefaa0374
ValeryP <>
2020-07-13 21:27:58 +0300

    Extend Test coverege of PersonServiceImpl to 100%.

--- src/test/java/com/niit/BookStore/service/impl/PersonServiceImplTest.java
+++ src/test/java/com/niit/BookStore/service/impl/PersonServiceImplTest.java
@@ -8,6 +8,9 @@ import com.niit.BookStore.service.CustomConversionService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.mockito.InjectMocks;
@@ -15,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,7 +42,7 @@ class PersonServiceImplTest {
     }
 
     @Test
-    public void verifyGetPersonBYIdFromRepository() {
+    public void verifyGetPersonByIdFromRepository() {
         Mockito.when(personRepository.findById(any())).thenReturn(Optional.of(new Person()));
         personService.getPersonById(1L);
         Mockito.verify(personRepository).findById(any());
@@ -70,4 +74,58 @@ class PersonServiceImplTest {
                 PERSON_NOT_FOUND_EXCEPTION_MESSAGE);
         Mockito.verify(personRepository).existsById(any());
     }
+
+    @Test
+    public void testGetAllPersons(){
+        List<Person> findAllUserList = new ArrayList<>(Arrays.asList(new Person(), new Person()));
+        Mockito.when(personRepository.findAll()).thenReturn(findAllUserList);
+        List<PersonDto> convertedResult = new ArrayList<>(Arrays.asList(new PersonDto(), new PersonDto()));
+        Mockito.when(conversionService.convert(findAllUserList, PersonDto.class))
+                .thenReturn(convertedResult);
+        List<PersonDto> result = personService.getAll();
+        assertSame(convertedResult, result);
+    }
+
+    @Test
+    public void testUpdatePersonByIdPersonNotFound(){
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(ItemNotFoundException.class, () -> personService.updatePerson(1L, new PersonDto()),
+                PERSON_NOT_FOUND_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    public void testUpdatePersonByIdUpdateAllField(){
+        Person originalPerson = new Person();
+        PersonDto personForUpdate = PersonDto.builder()
+                .id(1L)
+                .email("new_email@email.con")
+                .firstName("FN")
+                .lastName("LN")
+                .build();
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.of(originalPerson));
+
+        personService.updatePerson(personForUpdate.getId(), personForUpdate);
+
+        assertEquals(personForUpdate.getEmail(), originalPerson.getEmail());
+        assertEquals(personForUpdate.getFirstName(), originalPerson.getFirstName());
+        assertEquals(personForUpdate.getLastName(), originalPerson.getLastName());
+    }
+
+    @Test
+    public void testUpdatePersonByIdGetConvertedPerson(){
+        Person originalPerson = new Person();
+        PersonDto personForUpdate = PersonDto.builder()
+                .id(1L)
+                .build();
+        PersonDto personAfterUpdate = new PersonDto();
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.of(originalPerson));
+        Mockito.when(conversionService.convert(originalPerson, PersonDto.class)).thenReturn(personAfterUpdate);
+
+        PersonDto result = personService.updatePerson(personForUpdate.getId(), personForUpdate);
+        Mockito.verify(conversionService).convert(originalPerson, PersonDto.class);
+        assertSame(personAfterUpdate, result);
+
+    }
+
+
 }

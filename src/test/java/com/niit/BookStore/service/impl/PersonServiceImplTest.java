03d92792d709eb4a8e01d315c5061a0edc0ac0bf
ValeryP <>
2020-07-13 20:47:09 +0300

    Introduce first Unit tests

---
+++ src/test/java/com/niit/BookStore/service/impl/PersonServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.niit.BookStore.service.impl;
+
+import com.niit.BookStore.dto.PersonDto;
+import com.niit.BookStore.entiny.Person;
+import com.niit.BookStore.exception.ItemNotFoundException;
+import com.niit.BookStore.repository.PersonRepository;
+import com.niit.BookStore.service.CustomConversionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Optional;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceImplTest {
+    public static String PERSON_NOT_FOUND_EXCEPTION_MESSAGE = "Person not found";
+
+    @InjectMocks
+    private PersonServiceImpl personService;
+    @Mock
+    private PersonRepository personRepository;
+    @Mock
+    private CustomConversionService conversionService;
+
+    @Test
+    public void testGetPersonByIdPersonNotFound() {
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(ItemNotFoundException.class, () -> personService.getPersonById(1L),
+                PERSON_NOT_FOUND_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    public void verifyGetPersonBYIdFromRepository() {
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.of(new Person()));
+        personService.getPersonById(1L);
+        Mockito.verify(personRepository).findById(any());
+    }
+
+    @Test
+    public void verifyGetConvertPerson() {
+        Person expectedPerson = new Person();
+        PersonDto expectedPersonDto = new PersonDto();
+        Mockito.when(personRepository.findById(any())).thenReturn(Optional.of(expectedPerson));
+        Mockito.when(conversionService.convert(expectedPerson, PersonDto.class)).thenReturn(expectedPersonDto);
+        PersonDto result = personService.getPersonById(1L);
+        Mockito.verify(conversionService).convert(expectedPerson, PersonDto.class);
+        assertSame(expectedPersonDto, result);
+    }
+
+    @Test
+    public void testDeletePersonById() {
+        Mockito.when(personRepository.existsById(any())).thenReturn(Boolean.TRUE);
+        personService.deletePerson(1L);
+        Mockito.verify(personRepository).existsById(any());
+        Mockito.verify(personRepository).deleteById(any());
+    }
+
+    @Test
+    public void testDeletePersonByIdPersonNotFound() {
+        Mockito.when(personRepository.existsById(any())).thenReturn(Boolean.FALSE);
+        assertThrows(ItemNotFoundException.class, () -> personService.deletePerson(1L),
+                PERSON_NOT_FOUND_EXCEPTION_MESSAGE);
+        Mockito.verify(personRepository).existsById(any());
+    }
+}
